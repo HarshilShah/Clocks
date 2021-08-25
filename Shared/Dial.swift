bca3cd41700963c16720bdb006212d1aa78abacc
Harshil Shah <harshilshah1910@outlook.com>
2021-08-25 15:05:38 +0530

    Using dynamic colours to handle dark mode as well

--- Shared/Dial.swift
+++ Shared/Dial.swift
@@ -19,17 +19,17 @@ struct Dial: View {
 			let minuteHandRotation = Angle(degrees: minute/60 * 360 - 90)
 			
 			Circle()
-				.stroke(Color.black.opacity(0.2))
+				.stroke(Color.primary.opacity(0.2))
 				.overlay(
 					RoundedRectangle(cornerRadius: handWidth/2)
-						.fill(Color.black)
+						.fill(Color.primary)
 						.frame(width: proxy.size.width/2, height: handWidth)
 						.offset(x: proxy.size.width/4)
 						.rotationEffect(hourHandRotation)
 				)
 				.overlay(
 					RoundedRectangle(cornerRadius: handWidth/2)
-						.fill(Color.black)
+						.fill(Color.primary)
 						.frame(width: proxy.size.width/2 * 0.8, height: handWidth)
 						.offset(x: proxy.size.width/4 * 0.8)
 						.rotationEffect(minuteHandRotation)
