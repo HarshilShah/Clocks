99e2dd54366886bb2a5a67cdfdc3aa61f65c1e1e
Harshil Shah <harshilshah1910@outlook.com>
2021-08-25 15:05:29 +0530

    Fixed the way the live timer worked

--- Shared/ClockClient.swift
+++ Shared/ClockClient.swift
@@ -13,10 +13,23 @@ struct ClockClient {
 }
 
 extension ClockClient {
+	/// We delay the timer publisher by the amount of time until the start of
+	/// the next minute, and then prepend that timestamp to the timer's
+	/// emissions because it only fires after it's duration has completed
 	static var live: ClockClient {
 		ClockClient {
-			Timer.publish(every: 60, tolerance: nil, on: .main, in: .default, options: nil)
-				.autoconnect()
+			let now = Date()
+			let oneMinuteLater = Calendar.current.date(byAdding: .minute, value: 1, to: now)!
+			let startOfNextMinute = Calendar.current.dateInterval(of: .minute, for: oneMinuteLater)!.start
+			let delay = now.distance(to: startOfNextMinute)
+			
+			return Just(now)
+				.delay(for: .seconds(delay), scheduler: DispatchQueue.main)
+				.append(
+					Timer.publish(every: 60, tolerance: 0.01, on: .main, in: .default)
+						.autoconnect()
+						.prepend(startOfNextMinute)
+				)
 				.eraseToAnyPublisher()
 		}
 	}
